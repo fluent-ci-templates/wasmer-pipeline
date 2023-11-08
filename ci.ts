1af411c68ecf6454ca35f799fb7c35a1e98ac3a7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-08 06:25:21 +0000

    feat: handle pipeline arguments

--- ci.ts
+++ ci.ts
@@ -1,7 +1,7 @@
 import {
   build,
   deploy,
-} from "https://pkg.fluentci.io/wasmer_pipeline@v0.1.0/mod.ts";
+} from "https://pkg.fluentci.io/wasmer_pipeline@v0.2.0/mod.ts";
 
 await build(".");
 await deploy(".", Deno.env.get("WASMER_TOKEN"), true);
